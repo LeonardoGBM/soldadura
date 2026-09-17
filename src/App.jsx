6e9068bc73b44ad047d2301c683bd509c051f7af
Your Name <you@example.com>
2026-09-17 00:20:15 -0500

    feat:rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useState } from 'react'
 import './App.css'
 import Navbar from './parts/navbar'
 import Footer from './parts/footer'
 import Inicio from './pages/inicio'
 import Qsomos from './pages/qsomos'
+import { Form } from 'react-router-dom'
 function App() {
 
   return (
     <>
-   <Navbar/>
-    <main className="flex-grow-1">
-        <Qsomos/>
-    </main>
-   <Footer/>
+      <BrowserRouter>
+        <Navbar />
+        <main className="flex-grow-1">
+          <Routes>
+            <Route path='/' element={<Inicio />}/>
+            <Route path='/qsomos' element={<Qsomos/>} />
+            <Route path='/galeria' /> 
+            </Routes>
+        </main>
+        <Footer />
+      </BrowserRouter >
+
+
     </>
   )
 }
--- src/parts/navbar.jsx
+++ src/parts/navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import './navbar.css'
 const Navbar = () => {
   return (
@@ -6,9 +7,9 @@ const Navbar = () => {
       <nav className="navbar sticky-top">
         <a className="menu navbar-brand ms-5">Logo</a>
         <form className="form-inline">
-          <a href="" className='menu text-decoration-none m-2'><i className="bi bi-house-fill"></i> Inicio</a>
-          <a href="" className='menu text-decoration-none m-2'><i className="bi bi-people-fill"></i> ¿Quienes Somos?</a>
-          <a href="" className='menu text-decoration-none ms-2 me-5'><i className="bi bi-images"></i> Galeria</a>
+          <Link to='/' className='menu text-decoration-none m-2'><i className="bi bi-house-fill"></i> Inicio</Link>
+          <Link to='qsomos' className='menu text-decoration-none m-2'><i className="bi bi-people-fill"></i> ¿Quienes Somos?</Link>
+          <Link to='/' className='menu text-decoration-none ms-2 me-5'><i className="bi bi-images"></i> Galeria</Link>
             <button className="btn btn-outline-success my-2 my-sm-0 me-5" type="submit"><i className="bi bi-telephone-fill"></i> 0987175379</button>
         </form>
       </nav>
