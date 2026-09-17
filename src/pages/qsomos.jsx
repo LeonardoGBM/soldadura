519863aa7b2002036d2cb62fe447b64350623b38
Your Name <you@example.com>
2026-09-17 00:26:37 -0500

    feat:boton de cotisacion

--- src/pages/qsomos.jsx
+++ src/pages/qsomos.jsx
@@ -34,6 +34,15 @@ const Qsomos = () => {
                         </div>
                     </div>
                 </section>
+                <a
+                    href="https://wa.me/593987175379?text=Hola,%20me%20gustaría%20solicitar%20una%20cotización."
+                    target="_blank"
+                    rel="noreferrer"
+                    className="boton-cotizar"
+                >
+                    <i className="bi bi-whatsapp"></i>
+                    <span>Solicitar cotización</span>
+                </a>
             </main>
         </div>
     )
